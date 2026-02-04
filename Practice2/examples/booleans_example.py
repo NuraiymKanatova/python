51f7a9e802af3f80eebdba0e33dcbb0084992c9e
NuraiymKanatova <nuraek2006@gmail.com>
2026-02-04 18:03:18 +0500

    Add boolean example

---
+++ Practice2/examples/booleans_example.py
@@ -0,0 +1,21 @@
+print(True)
+print(False)
+
+x = 10
+y = 5
+
+print(x > y)
+print(x < y)
+print(x == y)
+
+if x > y:
+    print("x is greater than y")
+
+print(bool("Hello"))
+print(bool(1))
+print(bool(0))
+print(bool(""))
+
+print(x > 5 and x < 20)
+print(x < 5 or x > 10)
+print(not False)
