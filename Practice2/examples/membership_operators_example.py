2cba91f232da337783a0ef00b661edc7d5a46c94
NuraiymKanatova <nuraek2006@gmail.com>
2026-02-04 18:34:21 +0500

    Add membership operators example

---
+++ Practice2/examples/membership_operators_example.py
@@ -0,0 +1,15 @@
+numbers = [1, 2, 3, 4, 5]
+text = "hello"
+data = {"a": 1, "b": 2}
+
+print(3 in numbers)
+print(6 in numbers)
+
+print("h" in text)
+print("z" in text)
+
+print("a" in data)
+print(1 in data)
+
+print(10 not in numbers)
+print("x" not in text) 
