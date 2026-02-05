c732071fd60b5fae0668a67d82abc92385619758
NuraiymKanatova <nuraek2006@gmail.com>
2026-02-05 23:39:12 +0500

    Add unpack tuples example

---
+++ Practice2/examples/unpack_tuples_example.py
@@ -0,0 +1,11 @@
+# basic unpacking
+fruits = ("apple", "banana", "cherry")
+(a, b, c) = fruits
+print(a)
+print(b)
+print(c)
+
+# unpacking with different variable names
+x, y, z = fruits
+print(x, y, z)
+
