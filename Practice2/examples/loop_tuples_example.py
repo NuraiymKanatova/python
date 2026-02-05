fc2c624a19bf4a32c9631475b016c8c7427ef0ad
NuraiymKanatova <nuraek2006@gmail.com>
2026-02-05 21:06:33 +0500

    Add loop tuple items example

---
+++ Practice2/examples/loop_tuples_example.py
@@ -0,0 +1,9 @@
+fruits = ("apple", "banana", "cherry", "orange")
+
+print("For loop:")
+for fruit in fruits:
+    print(fruit)
+
+print("\nLoop by index:")
+for i in range(len(fruits)):
+    print(i, fruits[i])
