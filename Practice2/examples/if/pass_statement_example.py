5d66837cc4a2ad26365d8a8d2eebcc818183f02b
NuraiymKanatova <nuraek2006@gmail.com>
2026-02-07 17:33:56 +0500

    Move files to respective folder

---
+++ Practice2/examples/if/pass_statement_example.py
@@ -0,0 +1,14 @@
+a = 33
+b = 200
+
+if b > a:
+    pass
+
+# the pass statement is a null operation - nothing happens when it excecutes
+
+age = 16
+
+if age < 18:
+    pass  # TODO: add underage logic later
+else:
+    print("Access granted")
