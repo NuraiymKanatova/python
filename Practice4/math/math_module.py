49ea8158bf610ada2e98d7dbd29c89875622d623
NuraiymKanatova <nuraek2006@gmail.com>
2026-02-25 20:02:05 +0500

    Math Module Functions

---
+++ Practice4/math/math_module.py
@@ -0,0 +1,29 @@
+# pythom has also a buit-in module called math
+# which extends the list of mathematical functions
+# to use it, you must import the math module
+
+import math
+
+# when you have imported the math module
+# you can start using methods and constants of the module
+
+
+# the math.sqrt() method for example, returns the square root of a number
+import math
+x = math.sqrt(64)
+print(x)
+
+# the math.ceil() method rounds a number upwards to its nearest integer
+# the math.floor() method round a number downwards to its nearest integer
+import math
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x)     # returns 2
+print(y)     # returns 1
+
+# the math.pi() constant, returns the value of PI (3.14...)
+import math
+x = math.pi
+print(x)
