e2fbb1dfd4f0f0956e7fa7485f8ce9d779fde469
NuraiymKanatova <nuraek2006@gmail.com>
2026-02-25 19:48:59 +0500

    Built-in Math Functions

---
+++ Practice4/math/built_in_functions.py
@@ -0,0 +1,20 @@
+# python has a set of built-in functions
+# including an extensive math module
+# that allows you to perform mathemtical tasks on numbers
+
+
+# the min() and max() functions
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)               # 5
+print(y)               # 25
+
+# the abs() function return the absolute (positive) value of the specified number
+x = abs(-7.25)
+print(x)               # 7.25
+
+# the pow(x, y) function returns the value of x to the power of y
+# returns the value of 4 to the power of 3 (same as 4 * 4 * 4)
+x = pow(4, 3)
+print(x)               # 64
