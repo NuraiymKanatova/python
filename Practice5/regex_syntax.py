2967848c11689e9a520c7faf68593e46ab8da3fb
NuraiymKanatova <nuraek2006@gmail.com>
2026-03-06 18:17:43 +0500

    RegEx syntax explanation

---
+++ Practice5/regex_syntax.py
@@ -0,0 +1,12 @@
+# a RegEx, or Regular Expression, is a sequence of characters that forms a search pattern
+# RegEx can be used to check if a string contains the specified search pattern
+
+# python has a built-in package called re, which can be used to work with Regular Expressions
+# import re module
+import re
+
+# when you have imported re module, you can start using regular expressions
+import re
+
+txt = "The rain in Spain0"
+x = re.search("^The.*Spain$", txt)
